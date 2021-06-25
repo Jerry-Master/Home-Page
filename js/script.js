ae2eaac8b4cab1e044d3a1e313d4f02292a5e21f
Jose Pérez Cano <joseperez2000@hotmail.es>
2021-06-25 19:30:57 +0200

    body-certificates-all-screens

--- js/script.js
+++ js/script.js
@@ -4,7 +4,31 @@ $(document).on('ready', function() {
       vertical: false,
       centerMode: true,
       slidesToShow: 3,
-      slidesToScroll: 1
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }]
     });
     $(".lazy").slick({
       lazyLoad: 'ondemand', // ondemand progressive anticipated
