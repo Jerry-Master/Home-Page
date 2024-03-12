fd6b3297323ec6b7f88bb2d948106e5f3609033a
Jerry-Master <joseperez2000@hotmail.es>
2024-03-12 20:43:54 +0100

    update march 2024

--- js/script.js
+++ js/script.js
@@ -30,6 +30,45 @@ $(document).on('ready', function() {
           }
         }]
     });
+    $(".carousel-wide").slick({
+      dots: true,
+      vertical: false,
+      centerMode: true,
+      slidesToShow: 2,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 1200,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1,
+            infinite: true,
+            dots: true
+          }
+        },
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 2,
+            slidesToScroll: 1
+          }
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }]
+    });
+    $(".carousel-one").slick({
+      dots: true,
+      vertical: false,
+      centerMode: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      infinite: true,
+    });
     $(".lazy").slick({
       lazyLoad: 'ondemand', // ondemand progressive anticipated
       infinite: true
